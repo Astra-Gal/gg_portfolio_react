10243ba31d4431b53e5020a2e77476570fadb3e5
Astra-Gal <giulia.galastro@gmail.com>
2021-03-29 16:18:06 +0100

    prettier config sorted

--- src/components/SiteHeader.js
+++ src/components/SiteHeader.js
@@ -1,14 +1,16 @@
 import React from 'react';
 
 const SiteHeader = () => {
-    return (
-        <>
-            <div>
-                <h1><span className="my-first-name">giulia</span> <br></br> 
-                <span className="my-last-name" >galastro</span></h1>
-            </div>
-        </>
-    )
-}
+  return (
+    <>
+      <div>
+        <h1>
+          <span className="my-first-name">giulia</span> <br></br>
+          <span className="my-last-name">galastro</span>
+        </h1>
+      </div>
+    </>
+  );
+};
 
 export default SiteHeader;
