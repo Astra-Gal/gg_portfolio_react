097950bbac7934fbd73333fe6a268b7375363a0c
Astra-Gal <giulia.galastro@gmail.com>
2021-04-12 11:14:15 +0100

    weird flex, but ok!

--- src/components/SiteHeader.js
+++ src/components/SiteHeader.js
@@ -4,9 +4,9 @@ import '../styles/SiteHeader.css';
 const SiteHeader = () => {
   return (
     <>
-      <div>
+      <div className="my-full-name-and-title">
         <h1>
-          <span className="my-first-name">giulia</span> <br></br>
+          <span className="my-first-name">giulia </span>
           <span className="my-last-name">galastro</span>
         </h1>
         <h2>SOFTWARE DEVELOPER</h2>
